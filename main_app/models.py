e4f88b98ba81c491cc1ebee33ee8ceeb594ecfe9
KyleBarter <kylebarter4@gmail.com>
2023-06-15 20:39:04 +0100

    part 2, getting error in admin page

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Finch(models.Model):
+    species = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    color = models.CharField(max_length=100)
+
+
+def __str__(self):
+    return f'{self.color}, {self.species}, {self.location}, ({self.id})'
