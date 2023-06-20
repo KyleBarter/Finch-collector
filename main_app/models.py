4f8ea3ad59006dd84c3aa6f1a61fabb5e3435ffe
KyleBarter <kylebarter4@gmail.com>
2023-06-20 09:16:10 +0100

    not quite managed to get the toys creation working

--- main_app/models.py
+++ main_app/models.py
@@ -10,6 +10,16 @@ MEALS = (
 )
 
 # Create your models here.
+class Toy(models.Model):
+  name = models.CharField(max_length=50)
+  color = models.CharField(max_length=20)
+
+  def __str__(self):
+    return self.name
+
+  def get_absolute_url(self):
+    return reverse('toys_detail', kwargs={'pk': self.id})
+
 class Finch(models.Model):
     species = models.CharField(max_length=100)
     location = models.CharField(max_length=100)
